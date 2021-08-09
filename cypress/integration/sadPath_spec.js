aa453fdad32f1a79598cb295a568d2255efb4f0a
Alex Kio <alexandermartelkio@gmail.com>
2021-08-09 10:37:21 -0400

    Put Cypress it functions on single line

--- cypress/integration/sadPath_spec.js
+++ cypress/integration/sadPath_spec.js
@@ -24,8 +24,7 @@ describe('Sad path testing', () => {
     cy.contains('500')
   })
 
-  it("The error page should have a back button that takes the user home",
-  () => {
+  it("The error page should have a back button that takes the user home", () => {
     cy.intercept('GET', `http://ip-api.com/json/?fields=49600`, 
       {
         statusCode: 500, 
@@ -38,14 +37,12 @@ describe('Sad path testing', () => {
       .should('have.attr', 'href').and('includes', '/')
   })
 
-  it("The user is taken to an error page if they type in an invalid url",
-  () => {
+  it("The user is taken to an error page if they type in an invalid url", () => {
     cy.visit('http://localhost:3000/invalid')
     cy.url().should('include', '/404')
   })
 
-  it("The error page should have a back button that takes the user home",
-  () => {
+  it("The error page should have a back button that takes the user home", () => {
     cy.visit('http://localhost:3000/invalid')
     cy.url().should('include', '/404')
       .get('div[class="error-div"]').children('a').click()
