e9d8fc414f720f662781031453f43ad43f0be90b
Alex Kio <alexandermartelkio@gmail.com>
2021-08-18 14:59:33 -0400

    Convert Index to TypeScript file extension

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import { App } from './components/app/App';
-import { HashRouter } from 'react-router-dom';
 
 const router = <HashRouter> <App /> </HashRouter>;
 
